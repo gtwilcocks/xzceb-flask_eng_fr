a3c5cbaefe488d0a7cb5db2fe81459fea819b74c
Gerrit Wilcocks <gerrit.wilcocks@lsent.co.za>
2023-07-04 15:46:13 +0200

    Task4

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,17 +1,26 @@
+"""This module tests the translator module.
+"""
+
 import unittest
 
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
-class TestEnglishToFrench(unittest.TestCase): 
+class TestEnglishToFrench(unittest.TestCase):
+    """Tests the english_to_french function in the translator module.
+    """
+    def test1(self):
+        """Tests the english_to_french function in the translator module.
+        """
+        self.assertEqual(english_to_french('hello'), 'bonjour')
+        self.assertEqual(english_to_french("goodbye"), 'au revoir')
 
-    def test1(self): 
-        self.assertEqual(englishToFrench('hello'), 'bonjour') 
-        self.assertEqual(englishToFrench("goodbye"), 'au revoir') 
-    
- 
-class TestFrenchToEnglish(unittest.TestCase): 
-    def test2(self): 
-        self.assertEqual(frenchToEnglish('bonjour'), 'hello')         
-        self.assertEqual(frenchToEnglish('au revoir'), 'goodbye') 
-        
+class TestFrenchToEnglish(unittest.TestCase):
+    """Tests the french_to_english function in the translator module.
+    """
+    def test2(self):
+        """Tests the french_to_english function in the translator module.
+        """
+        self.assertEqual(french_to_english('bonjour'), 'hello')
+        self.assertEqual(french_to_english('au revoir'), 'goodbye')
+
 unittest.main()
